2439b9d0e24ba61c0764a40d79e98854aef521b3
EAGLEEARTH <ufukcicek199@gmail.com>
2023-04-22 20:19:50 +0300

    mp4 added

--- main.py
+++ main.py
@@ -1,14 +1,22 @@
 from pytube import YouTube
 import os
 
-def Download(link):
+def Download(link,yt_format):
     youtubeObject = YouTube(link)
-    youtubeObject = youtubeObject.streams.filter(only_audio=True).first()
     try:
-        destination = str("DownLoadList")
-        out_file = youtubeObject.download(output_path=destination)
-        base, ext = os.path.splitext(out_file)
-        new_file = base + '.mp3'
+        if yt_format == "mp3":
+            youtubeObject = youtubeObject.streams.filter(only_audio=True).first()
+            destination = str("DownLoadMp3List")
+            out_file = youtubeObject.download(output_path=destination)
+            base, ext = os.path.splitext(out_file)
+            new_file = base + '.mp3'
+
+        elif yt_format == "mp4":
+            youtubeObject = youtubeObject.streams.filter().get_highest_resolution()
+            destination = str("DownLoadMp4List")
+            out_file = youtubeObject.download(output_path=destination)
+            base, ext = os.path.splitext(out_file)
+            new_file = base + '.mp4'
         os.rename(out_file, new_file)
     except:
         print("An error has occurred")
@@ -20,14 +28,14 @@ link_list=[]
 with open('downloader_list.txt', 'r') as fh:
     link_list = [str(line) for line in fh]
 
-def working():
+def working(yt_format):
     while True:
         list_length = len(link_list)
         list_length = 0 if list_length == 0 else list_length
         for link in link_list: 
             list_length = len(link_list)
             try: 
-                Download(link)
+                Download(link,yt_format)
                 link_list.remove(link)
             except:
                 print(link)
@@ -40,9 +48,17 @@ get_one_link = str(input("Add a link or add links to the downloader_list.txt fil
 if "https://www.youtube.com/" in get_one_link:
     print("Link Added successfully")
     link_list.append(get_one_link)
-    working()
+    yt_format = str(input("what is the format to download(mp3 or mp4): "))
+    if yt_format == "mp3" or yt_format == "mp4":
+        working(yt_format)
+    else:
+        print("Not Working")
 else:
     print("Not Link Working, If the file is full it will work with")
-    working()
+    yt_format = str(input("what is the format to download(mp3 or mp4): "))
+    if yt_format == "mp3" or yt_format == "mp4":
+        working(yt_format)
+    else:
+        print("Not Working")
 
 
